3d5d48f32fb0b21bf2e5b937a404a7116cb38619
Md Ashed Dewan <111151213+ASHAD-AD@users.noreply.github.com>
2023-09-20 19:34:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	for(let i=0; i<str.length(); i++){
+		for(let j=1; j<str.;ength(); j++){
+			str.char[i] == str.char[j];
+			break;
+		}
+		console.log(str.char[i]);
+	}
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
